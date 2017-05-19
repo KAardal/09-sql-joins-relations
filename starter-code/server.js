f9f88c9756dec78e4b7ff908100bf7886d288a18
JohnScarrow <johna@scarrows.com>
2017-05-18 17:29:06 -0700

    almost done

--- starter-code/server.js
+++ starter-code/server.js
@@ -77,16 +77,20 @@ app.put('/articles/:id', function(request, response) {
   // an author_id property, so we can reference it from the request.body.
   // TODO: Add the required values from the request as data for the SQL query to interpolate
   client.query(
-    ``,
-    []
+    `UPDATE authors
+    SET author = $1, "authorUrl" = $2
+    WHERE author_id = $3;`,
+    [request.body.author, request.body.authorUrl, request.params.id]
   )
   .then(function() {
     // TODO: Write a SQL query to update an article record. Keep in mind that article records
     // now have an author_id, in addition to title, category, publishedOn, and body.
     // TODO: Add the required values from the request as data for the SQL query to interpolate
     client.query(
-      ``,
-      []
+      `UPDATE articles
+      SET title = $1, category = $2, "publishedOn" = $3, body = $4
+      WHERE article_id = $5;`,
+      [request.body.title, request.body.category, request.body.publishedOn, request.body.body, request.params.id]
     )
   })
   .then(function() {
